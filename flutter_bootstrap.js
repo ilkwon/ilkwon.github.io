ad9cc006fee050d7891e18eecbefe9213ff39d71
ilkwon <>
2025-04-04 14:51:17 +0900

    Auto deploy: 2025-04-04 14:51:16

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4078362285"
+    serviceWorkerVersion: "2135084069"
   }
 });
